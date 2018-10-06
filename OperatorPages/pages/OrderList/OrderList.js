d649de5b2dce8a7212a2e0c72ab9c5c1e4c5edd0
HBY <450932358@.com>
2018-10-06 17:53:55 +0800

    接线员订气订单列表+详情数据渲染

--- OperatorPages/pages/OrderList/OrderList.js
+++ OperatorPages/pages/OrderList/OrderList.js
@@ -1,32 +1,222 @@
-// pages/OrderList/OrderList.js
+let { baseUrl } = getApp().globalData
+const baseUrls = `${baseUrl}/Api/GasOrders/GetProcessedOrders`//获取全部订单列表接口
+const UntreatedUrls = `${baseUrl}/Api/GasOrders/GetWaitForProcessOrders`//获取待处理订单列表接口
+const Cancel = `${baseUrl}/Api/GasOrders/CancelOrder`//取消订单接口
+const Confirm = `${baseUrl}/Api/GasOrders/ReceiptOrder`//确认订单接口
+let app = getApp().globalData
+const utils = require("../../../utils/util.js")
+
+let Num = 2;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    navbar: ['全部订单','待处理订单','取消订单'],
+    navbar: ['全部订单', '待处理订单', '取消订单'],
     currentTab: 0,
-    waitconfirm:[
-      {Price:'1200',Phone:'13325468900',CustomeId:'348867486123',Address:'龙泉区某某镇某某路',OrderType:'系统订单'},
-      {Price:'1200',Phone:'13325468900',CustomeId:'348867486123',Address:'龙泉区某某镇某某路',OrderType:'系统订单'},
-    ],
-    confirm:[
-      {Price:'1200',Phone:'13325468900',CustomeId:'348867486123',Address:'龙泉区某某镇某某路',OrderType:'电话订单'},
-      {Price:'1200',Phone:'13325468900',CustomeId:'348867486123',Address:'龙泉区某某镇某某路',OrderType:'电话订单'},
-    ],
-    
+    waitconfirm: [],
+    Untreated: [],
+    CancelList: [],
+    pageIndex: 1,//当前页数，1代表第一页，以此类推
+    pageSize: 3,//每页返回的数据条数
+    ShowModal: false, //弹框按钮操控
+    orderId: "",
+    getText: ''
   },
-  //确定详情
-  onConfirmDetails(){
-    wx.navigateTo({
-      url: '/OperatorPages/pages/ConfirmDetails/ConfirmDetails',
+  //确认订单事件
+  conflrm(e) {
+    console.log(e)
+    let orderId = e.currentTarget.dataset.orderid
+    let customerId = e.currentTarget.dataset.customerid
+    wx.request({
+      url: Confirm,
+      data: {
+        Sign: "",
+        OrderId: orderId,
+        CustomerId: customerId
+      },
+      method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      // header: {}, // 设置请求的 header
+      success: function (res) {
+        console.log(res)
+      },
     })
   },
-  //待确定详情
-  onWaitconfirmDetails(){
-    wx.navigateTo({
-      url: '/OperatorPages/pages/WaitconfirmDetails/WaitconfirmDetails',
+  //取消订单显示弹窗事件
+  cancel(e) {
+    console.log(e)
+    let orderId = e.currentTarget.dataset.orderid
+    this.setData({
+      orderId,
+      ShowModal: true,
+    })
+  },
+  // 获取取消原因
+  getText: function (e) {
+    let getTexts = e.detail.value
+    this.setData({
+      getTexts
+    })
+  },
+  /**
+   * 隐藏模态对话框
+   */
+  HideModal: function () {
+    this.setData({
+      ShowModal: false
+    });
+  },
+  /**
+  * 对话框确认按钮点击事件
+  */
+  onConfirm: function (e) {
+    let _this = this
+    // 订单
+    let UserId = app.User.UserId
+    // 用户
+    let OrderId = _this.data.orderId
+    // tu款说明
+    let Explain = _this.data.getTexts
+    if (Explain !== "") {
+      wx.request({
+        url: Cancel,
+        data: {
+          Sign: "",
+          OrderId: OrderId,
+          UserId: UserId,
+          Explain: Explain
+        },
+        header: {
+          'content-type': 'application/json'
+        },
+        method: 'POST',
+        success: function (res) {
+          console.log(res)
+          _this.ObtainList()
+          wx.showToast({
+            title: "取消成功！",
+            duration: 2000
+          });
+          // 隐藏弹框
+          _this.HideModal()
+        },
+      })
+    } else {
+      wx.showToast({
+        title: "请填写取消原因",
+        duration: 1000
+      });
+    }
+  },
+  /**
+   * 对话框取消按钮点击事件
+   */
+  onCancel: function () {
+    this.HideModal();
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.ObtainList()
+  },
+
+  /**
+  * 页面相关事件处理函数--监听用户下拉动作
+  */
+  onPullDownRefresh() {
+    this.ObtainList()
+    this.Untreated()
+    wx.showToast({
+      title: "加载中",
+      icon: 'loading',
+      duration: 2000
+    });
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    let count = this.data.pageSize * Num;
+    this.setData({
+      pageIndex: this.data.pageIndex,
+      pageSize: count,
+    })
+    Num++;
+    this.ObtainList()
+    this.Untreated()
+    wx.showToast({
+      title: "加载中",
+      icon: 'loading',
+      duration: 2000
+    });
+  },
+  //待处理订单列表接口
+  Untreated() {
+    let this_ = this
+    let pageIndex = this_.data.pageIndex
+    let pageSize = this_.data.pageSize
+    wx.request({
+      url: UntreatedUrls,
+      data: {
+        sign: "",
+        pageIndex: pageIndex,
+        pageSize: pageSize,
+        enterpriseId: app.User.EnterpriseId,
+        orderStatus: 0
+      },
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      // header: {}, // 设置请求的 header
+      success: function (res) {
+        console.log(res.data.Data)
+        let data = res.data.Data
+        data.map(item => {
+          utils.Decrypt(item.Price)
+          utils.Decrypt(item.Phone)
+          utils.Decrypt(item.Address)
+        })
+        this_.setData({
+          Untreated: data
+        })
+      },
+    })
+  },
+  //获取全部订单列表接口
+  ObtainList() {
+    let this_ = this
+    let pageIndex = this_.data.pageIndex;
+    let pageSize = this_.data.pageSize;
+    wx.request({
+      url: baseUrls,
+      data: {
+        sign: "",
+        pageIndex: pageIndex,
+        pageSize: pageSize,
+        userId: app.User.UserId,
+        enterpriseId: app.User.EnterpriseId,
+        orderStatus: 0
+      },
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      // header: {}, // 设置请求的 header
+      success: function (res) {
+        console.log(res.data.Data)
+        let data = res.data.Data
+        let arr = [];
+        data.map(item => {//解密
+          utils.Decrypt(item.Price)
+          utils.Decrypt(item.Phone)
+          utils.Decrypt(item.Address)
+          if (item.Status == 100) {//遍历出取消订单
+            arr.push(item)
+          }
+        })
+        this_.setData({
+          waitconfirm: data,
+          CancelList: arr
+        })
+      },
     })
   },
   //导航控制
@@ -34,14 +224,24 @@ Page({
     this.setData({
       currentTab: e.currentTarget.dataset.idx
     })
+    let currentTab = this.data.currentTab
+    if (currentTab == 0) {
+      this.ObtainList()
+    } else if (currentTab == 1) {
+      this.Untreated()
+    }
   },
-  // 确认订单
-  conflrm:function(){
-
+  //确定详情
+  onConfirmDetails() {
+    wx.navigateTo({
+      url: '/OperatorPages/pages/ConfirmDetails/ConfirmDetails',
+    })
   },
-  // 取消订单
-  cancel:function(){
-
+  //订单详情
+  onWaitconfirmDetails(e) {
+    wx.navigateTo({
+      url: '/OperatorPages/pages/WaitconfirmDetails/WaitconfirmDetails?id=' + e.currentTarget.dataset.id,
+    })
   },
 
   // 底部导航跳转
@@ -61,12 +261,6 @@ Page({
     })
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
@@ -93,20 +287,6 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
 
   },
 
--- OperatorPages/pages/WaitconfirmDetails/WaitconfirmDetails.js
+++ OperatorPages/pages/WaitconfirmDetails/WaitconfirmDetails.js
@@ -1,20 +1,171 @@
-// pages/WaitconfirmDetails/WaitconfirmDetails.js
+let { baseUrl } = getApp().globalData
+const baseUrls = `${baseUrl}/Api/GasOrders/GetOrderInfo`//获取全部订单列表接口
+let app = getApp().globalData
+const Cancel = `${baseUrl}/Api/GasOrders/CancelOrder`//取消订单接口
+const Confirm = `${baseUrl}/Api/GasOrders/ReceiptOrder`//确认订单接口
+const utils = require("../../../utils/util.js")
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    ShowModal: false, //弹框按钮操控
+    orderId: "",
+    getText: '',
+    ID: "",
+    OperateExplain: "",
+    OperatorName:"",
+    FormatOperateTime:""
+  },
+  //确认订单事件
+  conflrm(e) {
+    let orderId = this.data.ID
+    let customerId = e.currentTarget.dataset.customerid
+    wx.request({
+      url: Confirm,
+      data: {
+        Sign: "",
+        OrderId: orderId,
+        CustomerId: customerId
+      },
+      method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      // header: {}, // 设置请求的 header
+      success: function (res) {
+        console.log(res)
+        if (res.data.Code == 200) {
+          wx.redirectTo({
+            url: '/OperatorPages/pages/OrderList/OrderList',
+          })
+        }
+      },
+    })
+  },
+  //取消订单显示弹窗事件
+  cancel(e) {
+    console.log(e)
+    let orderId = e.currentTarget.dataset.orderid
+    this.setData({
+      orderId,
+      ShowModal: true,
+    })
+  },
+  // 获取取消原因
+  getText: function (e) {
+    let getTexts = e.detail.value
+    this.setData({
+      getTexts
+    })
+  },
+  /**
+   * 隐藏模态对话框
+   */
+  HideModal: function () {
+    this.setData({
+      ShowModal: false
+    });
+  },
+  /**
+  * 对话框确认按钮点击事件
+  */
+  onConfirm: function (e) {
+    let _this = this
+    // 订单
+    let UserId = app.User.UserId
+    // 用户
+    let OrderId = _this.data.ID
+    // tu款说明
+    let Explain = _this.data.getTexts
+    if (Explain !== "") {
+      wx.request({
+        url: Cancel,
+        data: {
+          Sign: "",
+          OrderId: OrderId,
+          UserId: UserId,
+          Explain: Explain
+        },
+        header: {
+          'content-type': 'application/json'
+        },
+        method: 'POST',
+        success: function (res) {
+          console.log(res)
+          wx.showToast({
+            title: "取消成功！",
+            duration: 2000
+          });
+          // 隐藏弹框
+          _this.HideModal()
+          wx.redirectTo({
+            url: '/OperatorPages/pages/OrderList/OrderList',
+          })
+        },
+      })
+    } else {
+      wx.showToast({
+        title: "请填写取消原因",
+        duration: 1000
+      });
+    }
+  },
+  /**
+   * 对话框取消按钮点击事件
+   */
+  onCancel: function () {
+    this.HideModal();
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad(options) {
+    this.setData({
+      ID: options.id
+    })
+    this.QueryId(options)
   },
-
+  QueryId(options) {
+    let this_ = this
+    wx.request({
+      url: baseUrls,
+      data: {
+        sign: "",
+        orderId: options.id
+      },
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      // header: {}, // 设置请求的 header
+      success: function (res) {
+        console.log(res.data.Data)
+        let data = res.data.Data
+        utils.Decrypt(data.Contact)
+        utils.Decrypt(data.Phone)
+        utils.Decrypt(data.Address)
+        utils.Decrypt(data.Price)
+        data.OrderItems.map(item => {
+          utils.Decrypt(item.Quantity)
+          utils.Decrypt(item.Price)
+        })
+        let OperateExplain
+        let OperatorName
+        let FormatOperateTime
+        data.OrderTracks.map(item => {
+          if (item.TrackType == 100) {
+            OperateExplain = item.OperateExplain
+            OperatorName = item.OperatorName
+            FormatOperateTime = item.FormatOperateTime
+          }
+        })
+        this_.setData({
+          data,
+          OperateExplain,
+          OperatorName,
+          FormatOperateTime
+        })
+        console.log(this_.data)
+      },
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
